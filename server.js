85e8585b64e77f5c2ad657488746b1e0462f2528
CamiloMora <kalufau@hotmail.com>
2016-12-18 22:20:12 -0500

    instalación de gulp, pug, sass

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ var express = require('express');
 
 var app = express();
 
+//inclusión de vistas
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+
 app.get('/', function (req, res){
-	res.send('Hola mundo');
-} )
+	res.render('index')
+})
 
 app.listen(3000, function (err){
 	if (err) return console.log('Hubo un error'), process.exit(1);

eb93d560f788f4372fc57a0a33f313b10aa19f79
CamiloMora <kalufau@hotmail.com>
2016-12-18 20:34:14 -0500

    instalaciòn de express

---
+++ server.js
@@ -0,0 +1,14 @@
+//--save para declarar que tome la libreria como dependencia
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res){
+	res.send('Hola mundo');
+} )
+
+app.listen(3000, function (err){
+	if (err) return console.log('Hubo un error'), process.exit(1);
+
+	console.log('Clongram escuchando en el puerto 3000');
+})

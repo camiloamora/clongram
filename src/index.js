6b573fe040133b92b9de7fb176f573c0170a68cb
CamiloMora <kalufau@hotmail.com>
2016-12-23 15:42:52 -0500

    se agrego browserify, babelify

---
+++ src/index.js
@@ -0,0 +1,10 @@
+var numero = [ 400, 200, 1, -23 ];
+
+//sin ecmascript
+//var numerosMas1 = numero.map(function (numero){
+//	return numero + 1;
+//})
+
+var numerosMas1 = numero.map(n => n + 1)
+
+console.log(numerosMas1);

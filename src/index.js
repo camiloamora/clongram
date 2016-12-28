f98f1b9e73c4bd968b87e9712301c4c6366cd6e8
CamiloMora <kalufau@hotmail.com>
2016-12-27 22:30:42 -0500

    se agrego la libreria watchify

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-var numero = [ 400, 200, 1, -23 ];
+/*var numero = [ 400, 200, 1, -23 ];
 
 //sin ecmascript
 //var numerosMas1 = numero.map(function (numero){
@@ -7,4 +7,18 @@ var numero = [ 400, 200, 1, -23 ];
 
 var numerosMas1 = numero.map(n => n + 1)
 
-console.log(numerosMas1);
+console.log(numerosMas1);*/
+
+var page = require('page');
+
+var main = document.getElementById('main-container')
+
+page('/', function (ctx, next){
+	main.innerHTML = 'Home <a href="/signup">Signup</a>';
+})
+
+page('/signup', function (ctx, next){
+	main.innerHTML = 'Signup <a href="/">Home</a>';
+}) 
+
+page();

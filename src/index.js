12fe6c7c9ab0674e716db8a43e13ef2312f0b080
CamiloMora <kalufau@hotmail.com>
2017-01-09 19:48:16 -0500

    manejo de fechas para las publicaciones

--- src/index.js
+++ src/index.js
@@ -10,6 +10,11 @@ var numerosMas1 = numero.map(n => n + 1)
 console.log(numerosMas1);*/
 
 var page = require('page');
+var moment = require('moment');
+require('moment/locale/es');
+
+moment.locale('es');
+
 require('./homepage');
 require('./signup');
 require('./signin');
